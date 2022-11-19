e44e28fc0048277686893f4c27a2243681e823ea
JI20 <jonas.itt@tum.de>
2022-11-19 17:23:25 +0100

    Added login page

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,10 +1,16 @@
+import NavBar from "../../components/navBar";
 import "./style.css";
+import Illustration from "./Illustration";
+import Illustration2 from "./Illustration2";
 
 const LandingPage = () => {
   return (
-    <>
-      <h1>Das ist ein Test</h1>
-    </>
+    <div className="landingPage">
+      <NavBar authenticated={false} />
+      <h1>Your next stream is just 5 swipes away.</h1>
+      <Illustration2 className="illustration2" />
+      <Illustration className="illustration" />
+    </div>
   );
 };
 
