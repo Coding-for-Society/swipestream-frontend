a25d31296845e8e68534408acf4a774ff51eac8b
JI20 <jonas.itt@tum.de>
2022-11-20 09:58:07 +0100

    Fix unused import

--- src/pages/videoPage/index.jsx
+++ src/pages/videoPage/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { RequestTwo } from "../../api/ReuestTwoRecommendations";
 import VideoPlayer from "../../components/videoPlayer";
 import mergePreferences from "../../functions/mergePreferences/MergePreferences";
 import Arrow from "./Arrow";
